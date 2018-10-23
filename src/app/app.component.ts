1e24202fa7363b52ad25143975c94faca5a69c39
Tommy Gaessler <tommy.gaessler@gmail.com>
2018-10-22 23:54:29 -0600

    created proof of concept with basic auth and data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'snowday';
+export class AppComponent implements OnInit {
+
+  loading: boolean = false;
+  response: object = {};
+  mountainData: object = {};
+  loginForm: FormGroup;
+
+  constructor(fb: FormBuilder, public http: HttpClient) {
+    this.loginForm = fb.group({
+      'username': [null, Validators.required],
+      'password': [null, Validators.required]
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  login() {
+    this.mountainData = {};
+    this.loading = true;
+    this.http.post('https://ujj9l4tjj6.execute-api.us-east-1.amazonaws.com/prod/snowflakeLogin', {
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password,
+      remember: false
+    }).toPromise().then((data: any) => {
+      this.response = JSON.parse(data.body)['response'];
+      if(this.response['session_cookie']) {
+        this.getData(this.response['session_cookie']);
+      } else {
+        this.loading = false;
+      }
+    }).catch((error) => {
+      this.loading = false;
+      this.response['status'] = 'Server Error, Contact 303-565-0001';
+      console.log(error)
+    })
+  }
+
+  getData(cookie) {
+    this.http.post('https://ujj9l4tjj6.execute-api.us-east-1.amazonaws.com/prod/snowflakeData', {
+      'cookie': cookie
+    }).toPromise().then((data: any) => {
+      this.mountainData = JSON.parse(data.body);
+      this.loading = false;
+    }).catch((error) => {
+      this.loading = false;
+      this.response['status'] = 'Server Error, Contact 303-565-0001';
+      console.log(error)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule
+} from '@angular/material';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
